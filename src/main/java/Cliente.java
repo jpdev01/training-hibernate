ab8d5b947bbb645a9955da3b91bda1a6b73d2704
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-06 17:18:38 -0300

    Criação das classes iniciais e construção de Cliente.java

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,42 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Cliente {
+
+    private Integer id;
+    private String name;
+    private RG rg;
+    private Set<Livro> livros = new LinkedHashSet<Livro>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public RG getRg() {
+        return rg;
+    }
+
+    public void setRg(RG rg) {
+        this.rg = rg;
+    }
+
+    public Set<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(Set<Livro> livros) {
+        this.livros = livros;
+    }
+}
---
+++ src/main/java/Editora.java
@@ -0,0 +1,2 @@
+public class Editora {
+}
