f65f41df56a9c53f5977ed07c1093ff55275f218
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-07 15:54:51 -0300

    Criação de ProductDAO e ProductTest para testar as idas ao banco

--- src/main/java/model/bean/Product.java
+++ src/main/java/model/bean/Product.java
@@ -15,4 +15,74 @@ public class Product {
     // muitos produtos para uma categoria
     @ManyToOne
     private Category category;
+
+    public Product(){
+
+    }
+
+    public Product(Integer id, String name, double value, String description, int qtde, Category category) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+        this.description = description;
+        this.qtde = qtde;
+        this.category = category;
+    }
+
+    public Product(String name, double value, String description, int qtde, Category category) {
+        this.name = name;
+        this.value = value;
+        this.description = description;
+        this.qtde = qtde;
+        this.category = category;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQtde() {
+        return qtde;
+    }
+
+    public void setQtde(int qtde) {
+        this.qtde = qtde;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- src/test/java/categories/ProductTest.java
+++ src/test/java/categories/ProductTest.java
@@ -1,2 +1,31 @@
-package categories;public class ProductTest {
+package categories;
+
+import model.bean.Category;
+import model.bean.Product;
+import model.dao.CategoryDAO;
+import model.dao.ProductDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductTest {
+
+    private static ProductDAO dao = new ProductDAO();
+    private static CategoryDAO catgDao = new CategoryDAO();
+
+    public static void main(String[] args) {
+        saveTest();
+    }
+
+    public static void saveTest(){
+        List<Product> produtos = new ArrayList<Product>();
+
+        Category category = catgDao.findById(01);
+        produtos.add(new Product("Produto 1", 120.00, "Produto de teste", 01, category));
+
+        for (Product product : produtos){
+            dao.save(product);
+        }
+    }
+
 }
