fbc0aa6edbceef43fd9dacec7e762ea560239f01
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-07 07:54:07 -0300

    configurando persistence.xml

--- src/main/java/RG.java
+++ src/main/java/RG.java
@@ -1,2 +1,47 @@
+import bean.Cliente;
+
+import java.util.Date;
+
 public class RG {
+
+    private Integer id;
+    private String number;
+    private Cliente cliente;
+    private Date dataEspedicao;
+
+    public RG(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Date getDataEspedicao() {
+        return dataEspedicao;
+    }
+
+    public void setDataEspedicao(Date dataEspedicao) {
+        this.dataEspedicao = dataEspedicao;
+    }
 }
--- src/main/java/bean/Cliente.java
+++ src/main/java/bean/Cliente.java
@@ -1,11 +1,24 @@
+package bean;
+
+import javax.persistence.*;
 import java.util.LinkedHashSet;
 import java.util.Set;
-
+@Entity
 public class Cliente {
 
+    // IDENTIFY É MYSQL
+    // AUTO É DEFAULT
+    // AUTO VIRA AUTO INCREMENTO P/ MYSQL
+    // ORACLE SERIA SEQUENCE
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+    @OneToOne
     private RG rg;
+
+
     private Set<Livro> livros = new LinkedHashSet<Livro>();
 
     public Integer getId() {
