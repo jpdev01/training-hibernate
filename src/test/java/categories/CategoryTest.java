7ca63cea5f36192bf16fa9c03122d221273b0a75
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-07 11:15:02 -0300

    Adaptação para salvamento por for

--- src/test/java/categories/CategoryTest.java
+++ src/test/java/categories/CategoryTest.java
@@ -5,15 +5,32 @@ import model.bean.Category;
 import model.dao.CategoryDAO;
 
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryTest {
 
     public static void main(String[] args) {
+        saveTest();
+    }
+
+    public static void saveTest(){
+        List<Category> categories = new ArrayList<>();
         Category c = new Category("Tecnologias", "Tecnologias para seu conforto");
         Category c1 = new Category("Bebidas", "Bebidas geladas");
+        categories.add(c);
+        categories.add(c1);
+
+        for (Category cat : categories){
+           if (cat.getId() != null){
+               return;
+           }
+        }
 
         CategoryDAO ctgDAO = new CategoryDAO();
-        ctgDAO.save(c);
-        ctgDAO.save(c1);
+
+        for (Category cat : categories ){
+            ctgDAO.save(cat);
+        }
     }
 }
